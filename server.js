96fe90fd8fa7559e7e960a36ce5e7692b6aba301
Eric Gage <eric@ericgage.us>
2022-11-15 22:59:20 -0500

    server.js update - api working

---
+++ server.js
@@ -0,0 +1,26 @@
+const PORT = 8000;
+const axios = require("axios").default;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://themealdb.p.rapidapi.com/randomselection.php',
+        headers: {
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+          'X-RapidAPI-Host': 'themealdb.p.rapidapi.com'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+});
+
+app.listen(PORT, () => console.log('running on PORT ' + PORT));
