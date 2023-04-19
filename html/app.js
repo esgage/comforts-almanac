9fc37e89104619b0d3681ae5f7602c224951b4b1
Eric Gage <eric@ericgage.us>
2023-04-18 22:55:56 -0400

    recipe page desktop styles

--- html/app.js
+++ html/app.js
@@ -261,11 +261,13 @@ const loadRecipe = (recipe) => {
     recipeContainer.classList.add('recipe-container');
     recipeContainer.innerHTML = `
         <h1>${meal.strMeal}</h1>
-        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" height="600" width="600">
-        <h2>Category: <a href="" onClick='getCategory("${meal.strCategory}"); return false;'>${meal.strCategory}</a></h2>
-        <h2>Cuisine: <a href="" onClick='getArea("${meal.strArea}"); return false;'>${meal.strArea}</a></h2>
-        ${ingredients.outerHTML}
-        <ol><li>${formattedInstructions}</li></ol>
+        <div class="recipe-img"><img src="${meal.strMealThumb}" alt="${meal.strMeal}" height="600" width="600"></div>
+        <div class="recipe-classification">
+            <h2>Category: <a href="" onClick='getCategory("${meal.strCategory}"); return false;'>${meal.strCategory}</a></h2>
+            <h2>Cuisine: <a href="" onClick='getArea("${meal.strArea}"); return false;'>${meal.strArea}</a></h2>
+        </div>
+        <div class="recipe-ingredients">${ingredients.outerHTML}</div>
+        <div class="recipe-instructions"><ol><li>${formattedInstructions}</li></ol></div>
     `;
 
     contentContainer.append(recipeContainer);
